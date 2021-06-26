766da66f84852874621a19ba85a15eda39057b64
stefan hall <stefhall@comcast.net>
2021-06-25 20:46:24 -0700

    Updates to index model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,16 @@
 // import models
 const User = require('./User');
 const Vote = require('./Vote');
+
+Vote.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+User.belongsToMany(Vote, {
+    foreignKey: "vote_id",
+});
+
+module.exports = {
+    Vote,
+    User,
+};
