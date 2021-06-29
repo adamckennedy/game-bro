4d8b9b2d858b701c0858140f9d8906b370bdebcb
stefan hall <stefhall@comcast.net>
2021-06-28 19:31:40 -0700

    Updates to models and api routes to add comments and votes

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create Post Model
+class Post extends Model {}
+
+Post.init(  
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.String,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'vote',
+    }
+);
+
+module.exports = Post;
--- server.js
+++ server.js
@@ -12,6 +12,21 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use(routes);
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, function() {
