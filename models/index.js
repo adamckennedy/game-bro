38ed46b1e7b0397bda5be196b8ab3b0573afdb65
stefan hall <stefhall@comcast.net>
2021-06-28 20:46:53 -0700

    Updating models and routes

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const Post = require('./Post');
 const User = require('./User');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 
 User.belongsToMany(Post, {
@@ -31,6 +32,22 @@ Vote.belongsTo(User, {
   Post.hasMany(Vote, {
     foreignKey: 'post_id'
   });
+
+  User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
 
 module.exports = {
     Vote,
